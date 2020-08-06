dac181f0a8f35d48b48248d4760e3fa494649623
Kelsey Niederer <kelseyniederer12@gmail.com>
2020-08-05 19:50:17 -0500

    first commit

---
+++ src/main/java/org/launchcode/homeloancompare/HomeLoanCompareApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.homeloancompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLoanCompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeLoanCompareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/homeloancompare/HomeLoanCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.homeloancompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeLoanCompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
