dda2bd0e58a66d4fcf9504b80699d7f1b2a55954
Kelsey Niederer <kelseyniederer12@gmail.com>
2020-08-20 12:20:53 -0500

    added model binding

--- src/main/java/org/launchcode/homeloancompare/controllers/LoanController.java
+++ src/main/java/org/launchcode/homeloancompare/controllers/LoanController.java
@@ -4,10 +4,7 @@ import org.launchcode.homeloancompare.data.LoanInquiryData;
 import org.launchcode.homeloancompare.models.LoanInquiry;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,9 +25,8 @@ public class LoanController {
     }
 
     @PostMapping("new")
-    public String processNewLoanInquiryForm(@RequestParam String propertyAddress,
-    @RequestParam String borrowersName){
-        LoanInquiryData.add(new LoanInquiry(propertyAddress, borrowersName));
+    public String processNewLoanInquiryForm(@ModelAttribute LoanInquiry newLoanInquiry){
+        LoanInquiryData.add(newLoanInquiry);
         return "redirect:";
     }
 
