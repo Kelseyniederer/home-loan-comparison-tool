19da99378af2d020590973edab257bd815fe395b
Kelsey Niederer <kelseyniederer12@gmail.com>
2020-08-19 19:26:22 -0500

    added new loan inquiry form and form processing method to loan controller

--- src/main/java/org/launchcode/homeloancompare/controllers/LoanController.java
+++ src/main/java/org/launchcode/homeloancompare/controllers/LoanController.java
@@ -3,24 +3,33 @@ package org.launchcode.homeloancompare.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("loan")
+@RequestMapping("loans")
 public class LoanController {
 
+    private static List<String> loanInquiries = new ArrayList<>();
 
     @GetMapping
     public String displayAllLoanInquiries(Model model){
-        List<String> loanInquiries = new ArrayList<>();
-        loanInquiries.add("123 Sesame Street");
-        loanInquiries.add("456 McDonald Ave");
-        loanInquiries.add("789 Potomac Street");
         model.addAttribute("loanInquiries", loanInquiries);
+        return "loans/index";
+    }
 
-        return "loan/index";
+    @GetMapping("new")
+    public String renderNewLoanInquiryForm(){
+        return "loans/new";
+    }
+
+    @PostMapping("new")
+    public String processNewLoanInquiryForm(@RequestParam String propertyAddress){
+        loanInquiries.add(propertyAddress);
+        return "redirect:";
     }
 }
