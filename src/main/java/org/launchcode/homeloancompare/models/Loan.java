69766b57b5d38f7e5432ad15a7c41232ff6469f4
Kelsey Niederer <kelseyniederer12@gmail.com>
2020-09-16 19:36:42 -0500

    added LoanDetails class

--- src/main/java/org/launchcode/homeloancompare/models/Loan.java
+++ src/main/java/org/launchcode/homeloancompare/models/Loan.java
@@ -2,78 +2,60 @@ package org.launchcode.homeloancompare.models;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-import org.springframework.format.annotation.NumberFormat;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.Email;
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Date;
 
 
 @Entity
 public class Loan extends AbstractEntity{
 
-    @Size(min = 3, max = 75, message = "Address must be between 3 and 75 characters")
-    @NotEmpty(message = "Please enter property address")
-    private String subjectProperty;
 
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     @NotEmpty(message = "Please enter your name")
     private String borrowersName;
 
-    @NotEmpty(message = "Please enter your email address")
-    @Email(message = "Invalid Email")
-    private String borrowersEmail;
+    @Size(min = 3, max = 75, message = "Address must be between 3 and 75 characters")
+    @NotEmpty(message = "Please enter property address")
+    private String subjectProperty;
 
-    @NotNull(message = "Please enter your estimated credit score")
-    @NumberFormat
-    private Integer estimatedCreditScore;
+    @OneToOne(cascade = CascadeType.ALL)
+    @Valid
+    @NotNull
+    private LoanDetails loanDetails;
 
-    private Integer purchasePrice;
-
-    private Integer homeOwnersInsurance;
-
-
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @NotNull(message = "Occupancy Type is Required")
     private OccupancyCategory occupancyCategory;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @NotNull(message = "Property Type is Required")
     private PropertyCategory propertyCategory;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @NotNull(message = "Transaction Type is Required")
     private TransactionCategory transactionCategory;
 
-    private Date closingDate;
-
-    private Date firstPaymentDate;
-
-    public Loan(String subjectProperty, String borrowersName, String borrowersEmail,
-                Integer estimatedCreditScore, Integer purchasePrice, Integer homeOwnersInsurance,
-                TransactionCategory transactionCategory, PropertyCategory propertyCategory, OccupancyCategory occupancyCategory, Date closingDate,
-                Date firstPaymentDate) {
-        this.subjectProperty = subjectProperty;
+    public Loan(@Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters") @NotEmpty(message = "Please enter your name") String borrowersName, @Size(min = 3, max = 75, message = "Address must be between 3 and 75 characters") @NotEmpty(message = "Please enter property address") String subjectProperty, @Valid @NotNull LoanDetails loanDetails, @NotNull(message = "Occupancy Type is Required") OccupancyCategory occupancyCategory, @NotNull(message = "Property Type is Required") PropertyCategory propertyCategory, @NotNull(message = "Transaction Type is Required") TransactionCategory transactionCategory) {
         this.borrowersName = borrowersName;
-        this.borrowersEmail = borrowersEmail;
-        this.estimatedCreditScore = estimatedCreditScore;
-        this.purchasePrice = purchasePrice;
-        this.homeOwnersInsurance = homeOwnersInsurance;
+        this.subjectProperty = subjectProperty;
+        this.loanDetails = loanDetails;
         this.occupancyCategory = occupancyCategory;
         this.propertyCategory = propertyCategory;
         this.transactionCategory = transactionCategory;
-        this.closingDate = closingDate;
-        this.firstPaymentDate = firstPaymentDate;
     }
 
-    public Loan(){}
+    public Loan() {
+    }
 
     public String getSubjectProperty() {
         return subjectProperty;
@@ -82,48 +64,6 @@ public class Loan extends AbstractEntity{
     public void setSubjectProperty(String subjectProperty) {
         this.subjectProperty = subjectProperty;
     }
-
-
-    public String getBorrowersName() {
-        return borrowersName;
-    }
-
-    public void setBorrowersName(String borrowersName) {
-        this.borrowersName = borrowersName;
-    }
-
-    public String getBorrowersEmail() {
-        return borrowersEmail;
-    }
-
-    public void setBorrowersEmail(String borrowersEmail) {
-        this.borrowersEmail = borrowersEmail;
-    }
-
-    public Integer getEstimatedCreditScore() {
-        return estimatedCreditScore;
-    }
-
-    public void setEstimatedCreditScore(Integer estimatedCreditScore) {
-        this.estimatedCreditScore = estimatedCreditScore;
-    }
-
-    public Integer getPurchasePrice() {
-        return purchasePrice;
-    }
-
-    public void setPurchasePrice(Integer purchasePrice) {
-        this.purchasePrice = purchasePrice;
-    }
-
-    public Integer getHomeOwnersInsurance() {
-        return homeOwnersInsurance;
-    }
-
-    public void setHomeOwnersInsurance(Integer homeOwnersInsurance) {
-        this.homeOwnersInsurance = homeOwnersInsurance;
-    }
-
 
     public OccupancyCategory getOccupancyCategory() {
         return occupancyCategory;
@@ -149,20 +89,20 @@ public class Loan extends AbstractEntity{
         this.transactionCategory = transactionCategory;
     }
 
-    public Date getClosingDate() {
-        return closingDate;
+    public String getBorrowersName() {
+        return borrowersName;
     }
 
-    public void setClosingDate(Date closingDate) {
-        this.closingDate = closingDate;
+    public void setBorrowersName(String borrowersName) {
+        this.borrowersName = borrowersName;
     }
 
-    public Date getFirstPaymentDate() {
-        return firstPaymentDate;
+    public LoanDetails getLoanDetails() {
+        return loanDetails;
     }
 
-    public void setFirstPaymentDate(Date firstPaymentDate) {
-        this.firstPaymentDate = firstPaymentDate;
+    public void setLoanDetails(LoanDetails loanDetails) {
+        this.loanDetails = loanDetails;
     }
 
     @Override
---
+++ src/main/java/org/launchcode/homeloancompare/models/LoanDetails.java
@@ -0,0 +1,91 @@
+package org.launchcode.homeloancompare.models;
+
+
+import org.springframework.format.annotation.NumberFormat;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+public class LoanDetails extends AbstractEntity {
+
+    @NotEmpty(message = "Please enter your email address")
+    @Email(message = "Invalid Email")
+    private String borrowersEmail;
+
+    @NotNull(message = "Please enter your estimated credit score")
+    @NumberFormat
+    private Integer estimatedCreditScore;
+
+    private Integer purchasePrice;
+
+    private Integer homeOwnersInsurance;
+
+    private Date closingDate;
+
+    private Date firstPaymentDate;
+
+    public LoanDetails(@NotEmpty(message = "Please enter your email address") @Email(message = "Invalid Email") String borrowersEmail, @NotNull(message = "Please enter your estimated credit score") Integer estimatedCreditScore, Integer purchasePrice, Integer homeOwnersInsurance, Date closingDate, Date firstPaymentDate) {
+        this.borrowersEmail = borrowersEmail;
+        this.estimatedCreditScore = estimatedCreditScore;
+        this.purchasePrice = purchasePrice;
+        this.homeOwnersInsurance = homeOwnersInsurance;
+        this.closingDate = closingDate;
+        this.firstPaymentDate = firstPaymentDate;
+    }
+
+    public LoanDetails() {
+    }
+
+    public String getBorrowersEmail() {
+        return borrowersEmail;
+    }
+
+    public void setBorrowersEmail(String borrowersEmail) {
+        this.borrowersEmail = borrowersEmail;
+    }
+
+    public Integer getEstimatedCreditScore() {
+        return estimatedCreditScore;
+    }
+
+    public void setEstimatedCreditScore(Integer estimatedCreditScore) {
+        this.estimatedCreditScore = estimatedCreditScore;
+    }
+
+    public Integer getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(Integer purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public Integer getHomeOwnersInsurance() {
+        return homeOwnersInsurance;
+    }
+
+    public void setHomeOwnersInsurance(Integer homeOwnersInsurance) {
+        this.homeOwnersInsurance = homeOwnersInsurance;
+    }
+
+
+    public Date getClosingDate() {
+        return closingDate;
+    }
+
+    public void setClosingDate(Date closingDate) {
+        this.closingDate = closingDate;
+    }
+
+    public Date getFirstPaymentDate() {
+        return firstPaymentDate;
+    }
+
+    public void setFirstPaymentDate(Date firstPaymentDate) {
+        this.firstPaymentDate = firstPaymentDate;
+    }
+}
