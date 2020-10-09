7e816294cf6f80acf56cc01a8b51fb26040fac0b
Kelsey Niederer <kelseyniederer12@gmail.com>
2020-10-09 14:50:16 -0500

    Added ARM column

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,6 +1,18 @@
    function showInput() {
-        let zipCode = document.getElementById("zip").innerHTML;
-        console.log(zipCode);
+    var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://realtor.p.rapidapi.com/finance/rates?loc=" + document.getElementById("zip").innerHTML,
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "realtor.p.rapidapi.com",
+		"x-rapidapi-key": "cbd883036amsha5013fa20dd478ep1d45a2jsn327f8d284020"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
 
     };
 
